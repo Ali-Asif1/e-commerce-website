a39ec69268972df438a6ff0ac12346c1be78dfaa
Ali-Asif1 <aliasif1485@yahoo.com>
2024-12-09 19:22:47 +0500

    done

--- src/components/Update.tsx
+++ src/components/Update.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 export const Update = () => {
   return (
     <div className='container mx-auto mt-10'>
-        <div>
-            <p>STAY UPTO DATE ABOUT OUR LATEST OFFERS</p>
-            <div>
-                <button></button>
+        <div className='grid md:grid-cols-2 bg-black gap-8 h-[240px] md:h-[180px] text-center justify-between items-center rounded-3xl'>
+            <p className='text-white text-3xl mt-2 mb-2 md:text-[40px] font-bold'>STAY UPTO DATE ABOUT OUR LATEST OFFERS</p>
+            <div className='flex flex-col items-center gap-4 mb-4'>
+                <button className='bg-gray-100 h-12 w-[350px] md:w-[394px] rounded-3xl flex items-center pl-6 gap-2'><img src="/images/email.png" alt="" /><input type=" text" placeholder='Enter your email' /></button>
+                <button className='bg-gray-100 h-12 w-[350px] md:w-[394px] rounded-3xl flex items-center justify-center pl-6 gap-2 '><p className='font-semibold'>Subcribe to Newsletter</p></button>
             </div>
         </div>
     </div>
