4990b24d4245b5937a28b935451da3ab90e65880
Ali-Asif1 <aliasif1485@yahoo.com>
2025-02-04 11:02:01 +0500

    'change'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import  BestSelling  from "@/components/BestSelling";
 import { Hero } from "@/components/Hero";
 import NewArrivals from "@/components/NewArrivals";
 import { Tags } from "@/components/Tags";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
@@ -9,7 +10,9 @@ export default function Home() {
       <Hero />
       <Tags />
       <NewArrivals />
+      <Suspense fallback={<div>Loading Best Selling...</div>}>
       <BestSelling />
+    </Suspense>
      
     </div>
   );
